d909f9fdedf767924481c9d369beccc0d6c54fcd
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-07-21 20:16:30 +0530

    added shutdown function

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let startMenu=document.querySelector('.start-menu');
 let windowsLogo=document.querySelector('.windows-logo');
+let shutDownButton=document.querySelector('.shutdown-button');
+let windowsShutdownPage=document.querySelector('.windows-shutdown');
+let windowsOffPage=document.querySelector('.windows-off-main');
+let windowsOpeningTimePage=document.querySelector('.windows-opening')
 windowsLogo.addEventListener('click',()=>{
-    if(startMenu.style.bottom==='-700px'){
+    if(startMenu.style.bottom==='-1000px'){
         startMenu.style.bottom='65px';
     }else{
-        startMenu.style.bottom='-700px'
+        startMenu.style.bottom='-1000px'
     }
+})
+// shutting down stuff
+shutDownButton.addEventListener('click',()=>{
+    windowsShutdownPage.style.display='flex';
+    setTimeout(()=>{
+        windowsShutdownPage.style.display='none';
+        windowsOffPage.style.display='flex';
+        if(startMenu.style.bottom==='65px'){
+            startMenu.style.bottom='-1000px'
+        }
+    },5000);
+})
+document.querySelector('.power-button').addEventListener('click',()=>{
+    windowsOffPage.style.display='none';
+    windowsOpeningTimePage.style.display='flex';
+    setTimeout(()=>{
+        
+        windowsOpeningTimePage.style.display='none';
+    },3500);
 })
