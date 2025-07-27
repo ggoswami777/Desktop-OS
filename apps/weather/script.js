1a643bf9d8b120790aae1c52184cd38f1fd28bd4
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-07-27 12:04:02 +0530

    completed weather app

--- apps/weather/script.js
+++ apps/weather/script.js
@@ -1,10 +1,47 @@
 const BASE_URL =
-  "https://api.weatherapi.com/v1/current.json?key=1db180f23cb943889dd53144252707&q=";
+  "https://api.weatherapi.com/v1/current.json?key=1db180f23cb943889dd53144252707&q=delhi";
+const originalfunction = async () => {
+    let response = await fetch(BASE_URL);
+        let data = await response.json();
+        let icon = data.current.condition.icon;
+        let text = data.current.condition.text;
+        let temp_c = data.current.temp_c;
+        let wind_kph = data.current.wind_kph;
+        let humidity = data.current.humidity;
+        let last_updated = data.current.last_updated;
+        let country = data.location.country;
+        let name = data.location.name;
+  let originalHTML = `
+  <div class="header">
+        <input
+          class="input-bar"
+          type="text"
+          placeholder="Search city (e.g. Delhi)"
+        />
+        <div class="location-name">📍 ${name},${country}</div>
+      </div>
+      <div class="weather-content">
+        <div class="left-section">
+          <div class="temperature">${temp_c}</div>
+          <div class="weather-condition">${text}</div>
+          <div class="other-weather-details">
+            <p>Wind:${wind_kph} <br>Humidity:${humidity} <br>Last updated: ${last_updated}</p>
+          </div>
+        </div>
+        <div class="right-section">
+          <img id="weather-icon" src="${icon}" alt="Weather Icon">
+        </div>
+      </div>
+  `;
+  document.querySelector('.weatherContainer').innerHTML=originalHTML;
+  updateWeather();
+};
+originalfunction();
+
 const updateWeather = () => {
   let inputValue = document.querySelector(".input-bar");
   inputValue.addEventListener("keydown", async (e) => {
     if (e.key === "Enter") {
-        
       console.log(inputValue.value);
       const URL =
         `https://api.weatherapi.com/v1/current.json?key=1db180f23cb943889dd53144252707&q=` +
@@ -22,8 +59,8 @@ const updateWeather = () => {
         let wind_kph = data.current.wind_kph;
         let humidity = data.current.humidity;
         let last_updated = data.current.last_updated;
-        let country=data.location.country;
-        let name=data.location.name;
+        let country = data.location.country;
+        let name = data.location.name;
         let weatherHTML = `
             <div class="header">
         <input
@@ -49,7 +86,7 @@ const updateWeather = () => {
         document.querySelector(".weatherContainer").innerHTML = weatherHTML;
         console.log(data);
       } catch (err) {
-        let erroredHTML=`
+        let erroredHTML = `
             <div class="header">
         <input
           class="input-bar"
@@ -70,11 +107,10 @@ const updateWeather = () => {
           <img id="weather-icon" src="" alt="Weather Icon">
         </div>
       </div>
-        `
+        `;
       }
       updateWeather();
     }
   });
-  
 };
 updateWeather();
